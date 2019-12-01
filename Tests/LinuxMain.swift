4988d91982cfa4ec0fa0bfc7dc148c6dabe15fdd
Fred Appelman <fred@appelman.net>
2019-12-01 11:12:06 +0100

    Made compatible with Swift version 5.1

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import SwiftMD5Tests
+
+XCTMain([
+     testCase(SwiftMD5Tests.allTests)
+])
--- Tests/SwiftMD5Tests/SwiftMD5Tests.swift
+++ Tests/SwiftMD5Tests/SwiftMD5Tests.swift
@@ -1,7 +1,7 @@
 import XCTest
 import SwiftMD5
 
-class SwiftMD5Tests: XCTestCase {
+public class SwiftMD5Tests: XCTestCase {
     func testMD5() {
         XCTAssertEqual("The quick brown fox jumps over the lazy dog".md5, "9e107d9d372bb6826bd81d3542a419d6")
         XCTAssertEqual("The quick brown fox jumps over the lazy dog.".md5, "e4d909c290d0fb1ca068ffaddf22cbd0")
@@ -9,7 +9,7 @@ class SwiftMD5Tests: XCTestCase {
         XCTAssertEqual("md5".md5, "1bc29b36f623ba82aaf6724fd3b16718")
         XCTAssertEqual("abc".md5, "900150983cd24fb0d6963f7d28e17f72")
     }
-    
+
     func testMD5Checksum() {
         let strings = [
             "The quick brown fox jumps over the lazy dog",
@@ -18,13 +18,13 @@ class SwiftMD5Tests: XCTestCase {
             "md5",
             "abc"
         ]
-        
+
         for str in strings {
             let bytes = [Byte](str.utf8)
             XCTAssertEqual(str.md5, md5(bytes).checksum)
         }
     }
-    
+
     func testEncodeMD5Digest() {
         XCTAssertEqual("The quick brown fox jumps over the lazy dog".md5, encodeMD5(digest: [158, 16, 125, 157, 55, 43, 182, 130, 107, 216, 29, 53, 66, 164, 25, 214]))
         XCTAssertEqual("The quick brown fox jumps over the lazy dog.".md5, encodeMD5(digest: [228, 217, 9, 194, 144, 208, 251, 28, 160, 104, 255, 173, 223, 34, 203, 208]))
@@ -32,7 +32,7 @@ class SwiftMD5Tests: XCTestCase {
         XCTAssertEqual("md5".md5, encodeMD5(digest: [27, 194, 155, 54, 246, 35, 186, 130, 170, 246, 114, 79, 211, 177, 103, 24]))
         XCTAssertEqual("abc".md5, encodeMD5(digest: [144, 1, 80, 152, 60, 210, 79, 176, 214, 150, 63, 125, 40, 225, 127, 114]))
     }
-    
+
     func testMD5Digest() {
         XCTAssertEqual("The quick brown fox jumps over the lazy dog".md5Digest, [158, 16, 125, 157, 55, 43, 182, 130, 107, 216, 29, 53, 66, 164, 25, 214])
         XCTAssertEqual("The quick brown fox jumps over the lazy dog.".md5Digest, [228, 217, 9, 194, 144, 208, 251, 28, 160, 104, 255, 173, 223, 34, 203, 208])
@@ -40,4 +40,11 @@ class SwiftMD5Tests: XCTestCase {
         XCTAssertEqual("md5".md5Digest, [27, 194, 155, 54, 246, 35, 186, 130, 170, 246, 114, 79, 211, 177, 103, 24])
         XCTAssertEqual("abc".md5Digest, [144, 1, 80, 152, 60, 210, 79, 176, 214, 150, 63, 125, 40, 225, 127, 114])
     }
+
+    static var allTests = [
+        ("testMD5", testMD5),
+        ("testMD5Checksum", testMD5Checksum),
+        ("testEncodeMD5Digest", testEncodeMD5Digest),
+        ("testMD5Digest", testMD5Digest)
+    ]
 }
